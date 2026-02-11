01b01004f1ad71020bb88556f5e90a7d629dc12e
AshutoshPal9161 <palashutosh9161@gmail.com>
2026-02-11 23:59:37 +0530

    selecting-elements-in-javascript

--- script.js
+++ script.js
@@ -6,17 +6,17 @@
 // const cssImage = document.querySelectorAll('#css-image')
 // const cssImage = document.querySelector('.css-image')
 // const jsImage = document.querySelector('[alt="javascript roadmap"]')
-// const ul = document.querySelector('ul')
+const ul = document.querySelector('ul')
 
-// const allImages = document.querySelectorAll('img')
+const allImages = document.querySelectorAll('img')
 
-// const imageInsideUl = ul.querySelector('.css-image')
+const imageInsideUl = ul.querySelector('.css-image')
 
-// const imagesUrl = [
-//   'https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg',
-//   'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=',
-//   'https://media.istockphoto.com/id/1181366400/photo/in-the-hands-of-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-on.jpg?s=612x612&w=0&k=20&c=jWUMrHgjMY9zQXsAwZFb1jfM6KxZE16-IXI1bvehjQM=',
-// ]
+const imagesUrl = [
+  'https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg',
+  'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=',
+  'https://media.istockphoto.com/id/1181366400/photo/in-the-hands-of-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-on.jpg?s=612x612&w=0&k=20&c=jWUMrHgjMY9zQXsAwZFb1jfM6KxZE16-IXI1bvehjQM=',
+]
 
 // allImages[0].src = imagesUrl[0]
 // allImages[1].src = imagesUrl[1]
@@ -26,9 +26,9 @@
 //   allImages[i].src = imagesUrl[i]
 // }
 
-// allImages.forEach((image, i) => {
-//   image.src = imagesUrl[i]
-// })
+allImages.forEach((image, i) => {
+  image.src = imagesUrl[i]
+})
 
 
 
